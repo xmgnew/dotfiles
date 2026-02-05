df90d9ab0c2da20faf24a5f393aa6db9c0643ccd
expldfsh <xmgnew@gmail.com>
2026-02-05 18:14:29 -0500

    Add nvim config

---
+++ linux/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          on_attach = function(client)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+        },
+      },
+    },
+  },
+}
