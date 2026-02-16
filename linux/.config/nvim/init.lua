58f6f320eb38db0060850577e120ee0193bec32a
expldfsh <xmgnew@gmail.com>
2026-02-15 19:23:29 -0500

    update nvim config

--- linux/.config/nvim/init.lua
+++ linux/.config/nvim/init.lua
@@ -1,3 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  command = "checktime",
+})
