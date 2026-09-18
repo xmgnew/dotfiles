b9b0524b0e1865e2a17921757c5ce2b28a751894
expldfsh <xmgnew@gmail.com>
2026-09-17 21:13:25 -0400

    add <leader>td for toggle diagnostic in nvim

--- linux/.config/nvim/init.lua
+++ linux/.config/nvim/init.lua
@@ -11,8 +11,12 @@ vim.filetype.add({
     },
 })
 
+vim.keymap.set("n", "<leader>td", function()
+    local is_enabled = vim.diagnostic.is_enabled()
+    vim.diagnostic.enable(not is_enabled)
+end, { desc = "Toggle Diagnostics" })
+
 vim.opt.wrap = true
 vim.opt.linebreak = true -- Wrap at word boundaries rather than in the middle of a word
 vim.opt.textwidth = 0 -- Ensure textwidth does not interfere with visual wrapping
 vim.opt.shell = "/opt/homebrew/bin/fish"
-
